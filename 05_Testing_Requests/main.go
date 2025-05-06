d264fa571181c3129173f85abbfc7cfbca0223ec
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-05-06 08:51:11 +0700

    Build a REST API - 05 Testing Requests

---
+++ 05_Testing_Requests/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"gin_rest_api/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	// GET, POST, PUT, PATCH, DELETE
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+
+	server.Run(":9090")
+}
+
+func getEvents(context *gin.Context) {
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "Could not parse request data.",
+		})
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{
+		"message": "Event created!",
+		"event":   event,
+	})
+}
