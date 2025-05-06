2f9f72e8fcda33c3259db904c31b66b28a27381b
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-05-06 08:58:03 +0700

    Build a REST API - 06 Initializing A Database

---
+++ 11_Rest_Api/gin_rest_api/db/db.go
@@ -0,0 +1,19 @@
+package db
+
+import "database/sql"
+
+var DB *sql.DB
+
+func InitDB() {
+	var err error
+	DB, err = sql.Open("sqlite3", "api.db")
+
+	if err != nil {
+		panic("Could not connect to database.")
+	}
+
+	DB.SetMaxOpenConns(10)
+	DB.SetMaxIdleConns(5)
+
+	// createTables()
+}
